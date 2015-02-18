a1f6a8ad9807f068a8e6956d67e4c10c46900ef0
Peter Suschlik <ps@neopoly.de>
2015-02-18 16:09:46 +0100

    Add docs for ROM::CSV

--- lib/rom/csv/dataset.rb
+++ lib/rom/csv/dataset.rb
@@ -2,9 +2,15 @@ require 'rom/support/array_dataset'
 
 module ROM
   module CSV
+    # Dataset for CSV
+    #
+    # @api public
     class Dataset
       include ArrayDataset
 
+      # Convert each CSV::Row to a hash
+      #
+      # @api public
       def self.row_proc
         -> row { row.to_hash }
       end
--- lib/rom/csv/repository.rb
+++ lib/rom/csv/repository.rb
@@ -2,35 +2,102 @@ require 'rom/repository'
 require 'rom/csv/dataset'
 
 module ROM
+  # CSV support for ROM
+  #
+  # @example
+  #   require 'rom/csv'
+  #   require 'ostruct'
+  #
+  #   setup = ROM.setup(:csv, "./spec/fixtures/users.csv")
+  #   setup.relation(:users) do
+  #     def by_name(name)
+  #       dataset.find_all { |row| row[:name] == name }
+  #     end
+  #   end
+  #
+  #   class User < OpenStruct
+  #   end
+  #
+  #   setup.mappers do
+  #     define(:users) do
+  #       model User
+  #     end
+  #   end
+  #
+  #   rom = setup.finalize
+  #   p rom.read(:users).by_name('Jane').one
+  #   # => #<User id=2, name="Jane", email="jane@doe.org">
+  #
+  # Note: Currently, only read operations are supported.
+  #
+  # @api public
   module CSV
+    # Relation subclass of CSV adapter
+    #
+    # @example
+    #   class Users < ROM::Relation[:csv]
+    #   end
+    #
+    # @api public
     Relation = Class.new(ROM::Relation)
 
+    # CSV repository interface
+    #
+    # @api public
     class Repository < ROM::Repository
-      attr_reader :datasets
-
       # Expect a path to a single csv file which will be registered by rom to
       # the given name or :default as the repository.
       #
       # Uses CSV.table which passes the following csv options:
-      #  headers: true
-      #  converters: numeric
-      #  header_converters: :symbol
+      # * headers: true
+      # * converters: numeric
+      # * header_converters: :symbol
+      #
+      # @param path [String] path to csv
+      #
+      # @api private
+      #
+      # @see CSV.table
       def initialize(path)
         @datasets = {}
         @connection = ::CSV.table(path).by_row!
       end
 
+      # Return dataset with the given name
+      #
+      # @param name [String] dataset name
+      # @return [Dataset]
+      #
+      # @api public
       def [](name)
         datasets[name]
       end
 
+      # Register a dataset in the repository
+      #
+      # If dataset already exists it will be returned
+      #
+      # @param name [String] dataset name
+      # @return [Dataset]
+      #
+      # @api public
       def dataset(name)
         datasets[name] = Dataset.new(connection)
       end
 
+      # Check if dataset exists
+      #
+      # @param name [String] dataset name
+      #
+      # @api public
       def dataset?(name)
         datasets.key?(name)
       end
+
+      private
+
+      # @api private
+      attr_reader :datasets
     end
   end
 end
