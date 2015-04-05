813278c50989b44d2b65980a8041a94c6e4fb55b
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2015-04-05 13:08:01 +0200

    added tests for project and order methods in relations

--- spec/integration/repository_spec.rb
+++ spec/integration/repository_spec.rb
@@ -14,6 +14,14 @@ describe 'CSV repository' do
       def by_name(name)
         restrict(name: name)
       end
+
+      def only_name
+        project(:name)
+      end
+
+      def ordered
+        order(:name, :email)
+      end
     end
 
     class User
@@ -33,12 +41,33 @@ describe 'CSV repository' do
   end
 
   describe 'env#relation' do
-    it 'returns mapped object' do
+    it 'returns restricted and mapped object' do
       jane = rom.relation(:users).as(:entity).by_name('Jane').to_a.first
 
-      expect(jane.id).to eql(2)
+      expect(jane.id).to eql(3)
       expect(jane.name).to eql('Jane')
       expect(jane.email).to eql('jane@doe.org')
+    end
+
+    it 'returns specified attributes in mapped object' do
+      jane = rom.relation(:users).as(:entity).only_name.to_a.first
+
+      expect(jane.id).to be_nil
+      expect(jane.name).not_to be_nil
+      expect(jane.email).to be_nil
+    end
+
+    it 'return ordered attributes by name and email' do
+      results = rom.relation(:users).as(:entity).ordered.to_a
+
+      expect(results[0].name).to eql('Jane')
+      expect(results[0].email).to eq('jane@doe.org')
+
+      expect(results[1].name).to eql('Julie')
+      expect(results[1].email).to eq('julie.andrews@example.com')
+
+      expect(results[2].name).to eql('Julie')
+      expect(results[2].email).to eq('julie@doe.org')
     end
   end
 end
