ecc46628bb0700360314141a39f3a874c0b413d5
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2015-04-29 23:33:28 +0200

    Add update command

--- lib/rom/csv/commands.rb
+++ lib/rom/csv/commands.rb
@@ -1,1 +1,2 @@
 require 'rom/csv/commands/create'
+require 'rom/csv/commands/update'
---
+++ spec/integration/commands/update_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+require 'virtus'
+
+describe 'Commands / Updates' do
+  subject(:rom) { setup.finalize }
+
+  let(:original_path) { File.expand_path('./spec/fixtures/users.csv') }
+  let(:path) { File.expand_path('./spec/fixtures/testing.csv') }
+
+  # If :csv is not passed in the gateway is named `:default`
+  let(:setup) { ROM.setup(:csv, path) }
+
+  subject(:users) { rom.commands.users }
+
+  let(:relation) { rom.relations.users }
+  let(:first_data) { relation.by_id(1).to_a.first }
+  let(:new_data) { { name: 'Peter' } }
+  let(:output_data) do
+    [{ user_id: 1, name: 'Julie', email: 'tester@example.com' }]
+  end
+
+  before do
+    FileUtils.copy(original_path, path)
+
+    setup.relation(:users) do
+      def by_id(id)
+        restrict(user_id: id)
+      end
+    end
+
+    class User
+      include Virtus.model
+
+      attribute :id, Integer
+      attribute :name, String
+      attribute :email, String
+    end
+
+    setup.mappers do
+      define(:users) do
+        model User
+        register_as :entity
+      end
+    end
+
+    setup.commands(:users) do
+      define(:update)
+    end
+  end
+
+  it 'updates everything when there is no original tuple' do
+    result = users.try do
+      users.update.by_id(1).call(email: 'tester@example.com')
+    end
+
+    expect(result.value.to_a).to match_array(output_data)
+
+    # FIXME: reload! should not be necessary
+    rom.relation(:users).relation.dataset.reload!
+
+    result = rom.relation(:users).as(:entity).by_id(1).to_a.first
+    expect(result.email).to eql('tester@example.com')
+  end
+end
