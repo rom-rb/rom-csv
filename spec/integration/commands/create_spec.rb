b550fc08e7178dd1f11daf9e964fba1a9076d3b0
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2015-06-14 12:04:49 +0200

    Add possibility to generate fresh csv file

--- spec/integration/commands/create_spec.rb
+++ spec/integration/commands/create_spec.rb
@@ -14,53 +14,89 @@ describe 'Commands / Create' do
 
   subject(:users) { rom.commands.users }
 
-  before do
-    FileUtils.copy(original_path, path)
+  context 'when csv file exists' do
+    before do
+      FileUtils.copy(original_path, path)
 
-    setup.relation(:users)
+      setup.relation(:users)
 
-    setup.mappers do
-      define(:users) do
-        model User
-        register_as :entity
+      setup.mappers do
+        define(:users) do
+          model User
+          register_as :entity
+        end
+      end
+
+      setup.commands(:users) do
+        define(:create) do
+          result :one
+        end
+
+        define(:create_many, type: :create) do
+          result :many
+        end
       end
     end
 
-    setup.commands(:users) do
-      define(:create) do
-        result :one
+    it 'returns a single tuple when result is set to :one' do
+      result = users.try do
+        users.create.call(user_id: 4, name: 'John', email: 'john@doe.org')
+      end
+      expect(result.value).to eql(user_id: 4, name: 'John', email: 'john@doe.org')
+
+      result = rom.relation(:users).as(:entity).to_a
+      expect(result.count).to eql(4)
+    end
+
+    it 'returns tuples when result is set to :many' do
+      result = users.try do
+        users.create_many.call([
+          { user_id: 4, name: 'Jane', email: 'jane@doe.org' },
+          { user_id: 5, name: 'Jack', email: 'jack@doe.org' }
+        ])
       end
 
-      define(:create_many, type: :create) do
-        result :many
-      end
-    end
-  end
-
-  it 'returns a single tuple when result is set to :one' do
-    result = users.try do
-      users.create.call(user_id: 4, name: 'John', email: 'john@doe.org')
-    end
-    expect(result.value).to eql(user_id: 4, name: 'John', email: 'john@doe.org')
-
-    result = rom.relation(:users).as(:entity).to_a
-    expect(result.count).to eql(4)
-  end
-
-  it 'returns tuples when result is set to :many' do
-    result = users.try do
-      users.create_many.call([
+      expect(result.value.to_a).to match_array([
         { user_id: 4, name: 'Jane', email: 'jane@doe.org' },
         { user_id: 5, name: 'Jack', email: 'jack@doe.org' }
       ])
+
+      result = rom.relation(:users).as(:entity).to_a
+      expect(result.count).to eql(5)
+    end
+  end
+
+  context "when csv file doesn't exists" do
+    before do
+      FileUtils.rm(path) if File.exists?(path)
+      FileUtils.touch(path)
+
+      setup.relation(:users)
+
+      setup.mappers do
+        define(:users) do
+          model User
+          register_as :entity
+        end
+      end
+
+      setup.commands(:users) do
+        define(:create) do
+          result :one
+        end
+      end
     end
 
-    expect(result.value.to_a).to match_array([
-      { user_id: 4, name: 'Jane', email: 'jane@doe.org' },
-      { user_id: 5, name: 'Jack', email: 'jack@doe.org' }
-    ])
+    it 'returns tuples from new generated csv file' do
+      result = rom.relation(:users).as(:entity).to_a
+      expect(result.count).to eql(0)
 
-    result = rom.relation(:users).as(:entity).to_a
-    expect(result.count).to eql(5)
+      users.try do
+        users.create.call(user_id: 4, name: 'John', email: 'john@doe.org')
+      end
+
+      result = rom.relation(:users).as(:entity).to_a
+      expect(result.count).to eql(1)
+    end
   end
 end
